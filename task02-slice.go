dfa8b1063fefd850e239056c1532f2163c16a65d
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-05-08 22:15:13 +0200

    add task02

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,13 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+
+    
+	for i := len(input) - 1; i>= 0; i-- {
+	
+		result = append(result, input[i] )	
+	
+	}
+
 	return
 }
